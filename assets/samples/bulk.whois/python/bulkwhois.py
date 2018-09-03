1f196990febaf055379d6b426979c22a24b844ad
whoisxmlapi <support@whoisxmlapi.com>
2018-09-03 14:18:28 +0300

    Python sample updated.

--- assets/samples/bulk.whois/python/bulkwhois.py
+++ assets/samples/bulk.whois/python/bulkwhois.py
@@ -1,40 +1,22 @@
 try:
-    from urllib.request import urlopen
-    from urllib import request
+    from urllib.request import Request, urlopen
 except ImportError:
-    from urllib2 import urlopen
-    import urllib2 as request
-import json, time
+    from urllib2 import Request, urlopen
+from json import dumps, loads
+from time import sleep
 
-domains = ['whoisxmlapi.com', 'threatintelligenceplatform.com']
-username = 'Your bulk whois api username'
-password = 'Your bulk whois api password'
-output_format = 'json'
-interval = 10
+url = 'https://www.whoisxmlapi.com/BulkWhoisLookup/bulkServices/'
+(dom, enc) = ['whoisxmlapi.com', 'threatintelligenceplatform.com'], 'utf-8'
+(login, passw, fm, interval) = 'Bulk whois api login', 'password', 'json', 5
 
-bulkwhois_base_url = 'https://www.whoisxmlapi.com/BulkWhoisLookup/bulkServices/'
-data = {
-    "domains": domains, "password": password,  "username": username, 
-     "outputFormat": output_format
-}
-req = request.Request(bulkwhois_base_url + 'bulkWhois')
-req.add_header('Content-Type', 'application/json')
-response = urlopen(req, json.dumps(data).encode('utf-8'))
-response_data = json.loads(response.read().decode('utf-8'))
-print('Response: ' + json.dumps(response_data))
-del data['domains']
-data.update({
-   'requestId': response_data['requestId'], 'searchType': 'all', 'maxRecords': 1,
-   'startIndex': 1
-})
-recordsLeft = len(domains)
-req2 = request.Request(bulkwhois_base_url + 'getRecords')
-while recordsLeft > 0:
-    req2.add_header('Content-Type', 'application/json')
-    time.sleep(interval)
-    response = urlopen(req2, json.dumps(data).encode('utf-8'))
-    recordsLeft = json.loads(response.read().decode('utf-8'))['recordsLeft']
-data.update({'maxRecords': len(domains)})
-time.sleep(interval)
-response = urlopen(req2, json.dumps(data).encode('utf-8'))
-print(response.read().decode('utf-8'))
+d = {'domains': dom, 'password': passw, 'username': login, 'outputFormat': fm}
+req = Request(url + 'bulkWhois')
+ri = loads(urlopen(req, dumps(d).encode(enc)).read())['requestId']
+d.update({'requestId':ri, 'searchType':'all', 'maxRecords':1, 'startIndex':1})
+(recs, req, d) = len(dom), Request(url + 'getRecords'), d.pop('domains')
+
+while recs > 0:
+    sleep(interval)
+    recs = loads(urlopen(req, dumps(d).encode(enc)).read())['recordsLeft']
+d.update({'maxRecords': len(dom)})
+print(urlopen(req, dumps(d).encode(enc)).read())
